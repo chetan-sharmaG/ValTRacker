5c2ea0711d54f64c3fd57f76dfc7d3184a0051e3
unknown <chetansharma2-deloitte>
2024-03-16 11:22:35 +0530

    16-03-2024 11:22am

--- Mapsjavascipt.js
+++ Mapsjavascipt.js
@@ -1,120 +1,345 @@
+
+function makeHidden(element) {
+
+    var ele = document.getElementById(element);
+    ele.style.visibility = 'hidden';
+}
+function makeVisible(element) {
+    var ele = document.getElementById(element);
+    ele.style.visibility = 'visible';
+}
 document.getElementById('lotus').addEventListener('mouseenter', function () {
+    var hoverClip = document.getElementById('hoverClip');
+    
     zoomToPath('lotus');
+    makeHidden('INDIA');
+    var dta = document.querySelector('.lc');
+    dta.style.visibility = 'visible';
+    document.querySelectorAll('.indiaLine').forEach(function (line) {
+        line.style.visibility = 'visible';
+        line.classList.add('animate-in');
+      
+    });
+    hoverClip.play();
     document.getElementById('indiaCircle').style.visibility = 'visible';
 
 });
 document.getElementById('lotus').addEventListener('mouseleave', function () {
+    var hoverClip = document.getElementById('hoverClip');
     zoomOut();
+    makeVisible('INDIA');
+    var dta = document.querySelector('.lc');
+    dta.style.visibility = 'hidden';
+    document.querySelectorAll('.indiaLine').forEach(function (line) {
+        line.style.visibility = 'hidden';
+        line.classList.remove('animate-in');
+        
+    }); 
     document.getElementById('indiaCircle').style.visibility = 'hidden';
 });
 document.getElementById('bind').addEventListener('mouseenter', function () {
     zoomToPath('bind');
+    makeHidden('Morocco');
+    var dta = document.querySelector('.bic');
+    dta.style.visibility = 'visible';
+    document.querySelectorAll('.bindLine').forEach(function (line) {
+        line.style.visibility = 'visible';
+        line.classList.add('animate-in');
+       
+    });
     document.getElementById('bindCircle').style.visibility = 'visible';
 
 });
 document.getElementById('bind').addEventListener('mouseleave', function () {
     zoomOut();
+    makeVisible('Morocco');
+    var dta = document.querySelector('.bic');
+        dta.style.visibility = 'hidden';
+    document.querySelectorAll('.bindLine').forEach(function (line) {
+        line.style.visibility = 'hidden';
+        line.classList.remove('animate-in');
+        
+    });
     document.getElementById('bindCircle').style.visibility = 'hidden';
 });
 document.getElementById('ascent').addEventListener('mouseenter', function () {
     zoomToPath('ascent');
+    makeHidden('Italy');
+    var dta = document.querySelector('.ac');
+        dta.style.visibility = 'visible';
+    document.querySelectorAll('.ascentLine').forEach(function (line) {
+        line.style.visibility = 'visible';
+        line.classList.add('animate-in');
+        
+    });
     document.getElementById('ascentCircle').style.visibility = 'visible';
 });
 document.getElementById('ascent').addEventListener('mouseleave', function () {
     zoomOut();
+    makeVisible('Italy');
+    var dta = document.querySelector('.ac');
+        dta.style.visibility = 'hidden';
+    document.querySelectorAll('.ascentLine').forEach(function (line) {
+        line.style.visibility = 'hidden';
+        line.classList.remove('animate-in');
+        
+    });
     document.getElementById('ascentCircle').style.visibility = 'hidden';
 });
 document.getElementById('haven').addEventListener('mouseenter', function () {
     zoomToPath('haven');
+    makeHidden('BHUTAN');
+    var dta = document.querySelector('.hc');
+        dta.style.visibility = 'visible';
+    document.querySelectorAll('.havenLine').forEach(function (line) {
+        line.style.visibility = 'visible';
+        line.classList.add('animate-in');
+        
+    });
     document.getElementById('havenCircle').style.visibility = 'visible';
 });
 document.getElementById('haven').addEventListener('mouseleave', function () {
     zoomOut();
+    makeVisible('BHUTAN');
+    var dta = document.querySelector('.hc');
+    dta.style.visibility = 'hidden';
+    document.querySelectorAll('.havenLine').forEach(function (line) {
+        line.style.visibility = 'hidden';
+        line.classList.remove('animate-in');
+       
+    });
     document.getElementById('havenCircle').style.visibility = 'hidden';
 });
 document.getElementById('breeze').addEventListener('mouseenter', function () {
     zoomToPath('breeze');
+    makeHidden('barmuda');
+    var dta = document.querySelector('.bc');
+        dta.style.visibility = 'visible';
+    document.querySelectorAll('.breezeLine').forEach(function (line) {
+        line.style.visibility = 'visible';
+        line.classList.add('animate-in');
+        
+    });
     document.getElementById('breezeCircle').style.visibility = 'visible';
 });
 document.getElementById('breeze').addEventListener('mouseleave', function () {
     zoomOut();
+    makeVisible('barmuda');
+    var dta = document.querySelector('.bc');
+        dta.style.visibility = 'hidden';
+    document.querySelectorAll('.breezeLine').forEach(function (line) {
+        line.style.visibility = 'hidden';
+        line.classList.remove('animate-in');
+        
+    });
     document.getElementById('breezeCircle').style.visibility = 'hidden';
 });
 document.getElementById('pearl').addEventListener('mouseenter', function () {
     zoomToPath('pearl');
+    makeHidden('Portugal');
+    var dta = document.querySelector('.pc');
+    dta.style.visibility = 'visible';
+    document.querySelectorAll('.pearlLine').forEach(function (line) {
+        line.style.visibility = 'visible';
+        line.classList.add('animate-in');
+       
+    });
     document.getElementById('pearlCircle').style.visibility = 'visible';
 });
 document.getElementById('pearl').addEventListener('mouseleave', function () {
     zoomOut();
+    makeVisible('Portugal');
+    var dta = document.querySelector('.pc');
+        dta.style.visibility = 'hidden';
+    document.querySelectorAll('.pearlLine').forEach(function (line) {
+        line.style.visibility = 'hidden';
+        line.classList.remove('animate-in');
+        
+    });
     document.getElementById('pearlCircle').style.visibility = 'hidden';
 });
 document.getElementById('icebox').addEventListener('mouseenter', function () {
     zoomToPath('icebox')
+    makeHidden('Russia');
+    var dta = document.querySelector('.ic');
+        dta.style.visibility = 'visible';
+    document.querySelectorAll('.iceboxLine').forEach(function (line) {
+        line.style.visibility = 'visible';
+        line.classList.add('animate-in');
+        
+    });
     document.getElementById('iceboxCircle').style.visibility = 'visible';
 });
 document.getElementById('icebox').addEventListener('mouseleave', function () {
     zoomOut();
+    makeVisible('Russia');
+    var dta = document.querySelector('.ic');
+    dta.style.visibility = 'hidden';
+    document.querySelectorAll('.iceboxLine').forEach(function (line) {
+        line.style.visibility = 'hidden';
+        line.classList.remove('animate-in');
+      
+    });
     document.getElementById('iceboxCircle').style.visibility = 'hidden';
 });
 document.getElementById('split').addEventListener('mouseenter', function () {
     zoomToPath('split')
+    makeHidden('Tokyo');
+    var dta = document.querySelector('.sc');
+        dta.style.visibility = 'visible';
+    document.querySelectorAll('.splitLine').forEach(function (line) {
+        line.style.visibility = 'visible';
+        line.classList.add('animate-in');
+        
+    });
     document.getElementById('splitCircle').style.visibility = 'visible';
 });
 document.getElementById('split').addEventListener('mouseleave', function () {
     zoomOut();
+    makeVisible('Tokyo')
+    var dta = document.querySelector('.sc');
+        dta.style.visibility = 'hidden';
+    document.querySelectorAll('.splitLine').forEach(function (line) {
+        line.style.visibility = 'hidden';
+        line.classList.remove('animate-in');
+        
+    });
     document.getElementById('splitCircle').style.visibility = 'hidden';
 });
 document.getElementById('sunset').addEventListener('mouseenter', function () {
-    zoomToPath('sunset')
+    zoomToPath('sunset');
+    makeHidden('UsaSunset');
+    var dta = document.querySelector('.suc');
+        dta.style.visibility = 'visible';
+    document.querySelectorAll('.sunsetLine').forEach(function (line) {
+        line.style.visibility = 'visible';
+        line.classList.add('animate-in');
+        
+    });
     document.getElementById('sunsetCircle').style.visibility = 'visible';
 });
 document.getElementById('sunset').addEventListener('mouseleave', function () {
     zoomOut();
+    makeVisible('UsaSunset');
+    var dta = document.querySelector('.suc');
+    dta.style.visibility = 'hidden';
+    document.querySelectorAll('.sunsetLine').forEach(function (line) {
+        line.style.visibility = 'hidden';
+        line.classList.remove('animate-in');
+        
+    });
     document.getElementById('sunsetCircle').style.visibility = 'hidden';
 });
 document.getElementById('fracture').addEventListener('mouseenter', function () {
-    zoomToPath('fracture')
+    zoomToPath('fracture');
+    makeHidden('UsaFracture');
+    var dta = document.querySelector('.fc');
+        dta.style.visibility = 'visible';
+    document.querySelectorAll('.fractureLine').forEach(function (line) {
+        line.style.visibility = 'visible';
+        line.classList.add('animate-in'); 
+        
+    });
     document.getElementById('fractureCircle').style.visibility = 'visible';
 });
 document.getElementById('fracture').addEventListener('mouseleave', function () {
     zoomOut();
+    makeVisible('UsaFracture');
+    var dta = document.querySelector('.fc');
+        dta.style.visibility = 'hidden';
+    document.querySelectorAll('.fractureLine').forEach(function (line) {
+        line.style.visibility = 'hidden';
+        line.classList.remove('animate-in');
+        
+    });
     document.getElementById('fractureCircle').style.visibility = 'hidden';
 });
 
-function zoomOut() {
-    // var svg = document.getElementById('map');
-    // svg.setAttribute('viewBox', '-221.46 -221.46 1822.433 911.283');
-    var svg = document.getElementById('map');
-    var currentViewBox = svg.getAttribute('viewBox').split(' ').map(Number);
-    var defaultViewBox = [-221.46, -221.46, 1822.433, 911.283];
-  
-    // Duration of the animation in milliseconds
-    var duration = 1000;
-    var startTime = null;
+// function zoomOut() {
+//     // var svg = document.getElementById('map');
+//     // svg.setAttribute('viewBox', '-221.46 -221.46 1822.433 911.283');
+//     var svg = document.getElementById('map');
+//     var currentViewBox = svg.getAttribute('viewBox').split(' ').map(Number);
+//     var defaultViewBox = [-221.46, -221.46, 1822.433, 911.283];
 
-    function animateZoom(timestamp) {
-        if (!startTime) startTime = timestamp;
-        var elapsedTime = timestamp - startTime;
-        var progress = Math.min(elapsedTime / duration, 1);
+//     // Duration of the animation in milliseconds
+//     var duration = 1000;
+//     var startTime = null;
 
-        // Apply an ease-in-out function to the progress
-        progress = progress < 0.5 ? 2 * progress * progress : -1 + (4 - 2 * progress) * progress;
+//     function animateZoom(timestamp) {
+//         if (!startTime) startTime = timestamp;
+//         var elapsedTime = timestamp - startTime;
+//         var progress = Math.min(elapsedTime / duration, 1);
 
-        // Interpolate between the current and default viewBox values
-        var intermediateViewBox = currentViewBox.map(function(start, index) {
-            return start + (defaultViewBox[index] - start) * progress;
-        });
+//         // Apply an ease-in-out function to the progress
+//         progress = progress < 0.5 ? 2 * progress * progress : -1 + (4 - 2 * progress) * progress;
 
-        svg.setAttribute('viewBox', intermediateViewBox.join(' '));
+//         // Interpolate between the current and default viewBox values
+//         var intermediateViewBox = currentViewBox.map(function (start, index) {
+//             return start + (defaultViewBox[index] - start) * progress;
+//         });
 
-        if (progress < 1) {
-            requestAnimationFrame(animateZoom);
-        }
-    }
+//         svg.setAttribute('viewBox', intermediateViewBox.join(' '));
 
-    requestAnimationFrame(animateZoom);
-}
+//         if (progress < 1) {
+//             requestAnimationFrame(animateZoom);
+//         }
+//     }
 
+//     requestAnimationFrame(animateZoom);
+// }
+
+// function zoomToPath(location) {
+//     var svg = document.getElementById('map');
+//     const locations = {
+//         lotus: "710.1619873046875 99.5 450 400",
+//         haven: "897.1689999999999 80.018 400 400",
+//         split: "1107.4270000000001 2.3119999999999976 400 400",
+//         icebox: "900.778 -229.626 500 500",
+//         bind: "135.18599999999992 -18.741 600 600",
+//         pearl: "225.18599999999992 -58.741 500 500",
+//         ascent: "421.216 -69.15100000000001 400 400",
+//         breeze: "77.405 70.76499999999999 400 400",
+//         fracture: "-121.595 -29.235000000000014 500 500",
+//         sunset: "-121.595 -29.235000000000014 500 500",
+//     };
+
+
+//     //var viewBoxValue = locations[location] || '';
+//     // svg.setAttribute('viewBox', viewBoxValue);
+//     // console.error(viewBoxValue);
+//     var newViewBoxValue = locations[location] || '';
+//     let currentViewBox = svg.getAttribute('viewBox').split(' ').map(Number);
+//     let newViewBox = newViewBoxValue.split(' ').map(Number);
+
+//     // Duration of the animation in milliseconds
+//     let duration = 1000;
+//     let startTime = null;
+
+//     function animateZoom(timestamp) {
+//         if (!startTime) startTime = timestamp;
+//         let elapsedTime = timestamp - startTime;
+
+//         // Calculate the progress
+//         let progress = Math.min(elapsedTime / duration, 1);
+
+//         // Apply an ease-in-out function to the progress
+//         progress = progress < 0.5 ? 2 * progress * progress : -1 + (4 - 2 * progress) * progress;
+
+//         // Interpolate between the current and new viewBox values
+//         let intermediateViewBox = currentViewBox.map((start, index) => {
+//             return start + (newViewBox[index] - start) * progress;
+//         });
+
+//         svg.setAttribute('viewBox', intermediateViewBox.join(' '));
+
+//         if (progress < 1) {
+//             requestAnimationFrame(animateZoom);
+//         }
+//     }
+
+//     requestAnimationFrame(animateZoom);
+// }
 function zoomToPath(location) {
     var svg = document.getElementById('map');
     const locations = {
@@ -130,10 +355,6 @@ function zoomToPath(location) {
         sunset: "-121.595 -29.235000000000014 500 500",
     };
 
-
-    //var viewBoxValue = locations[location] || '';
-    // svg.setAttribute('viewBox', viewBoxValue);
-    // console.error(viewBoxValue);
     var newViewBoxValue = locations[location] || '';
     let currentViewBox = svg.getAttribute('viewBox').split(' ').map(Number);
     let newViewBox = newViewBoxValue.split(' ').map(Number);
@@ -150,11 +371,47 @@ function zoomToPath(location) {
         let progress = Math.min(elapsedTime / duration, 1);
 
         // Apply an ease-in-out function to the progress
-        progress = progress < 0.5 ? 2 * progress * progress : -1 + (4 - 2 * progress) * progress;
+        progress = easeInOutQuad(progress);
 
         // Interpolate between the current and new viewBox values
         let intermediateViewBox = currentViewBox.map((start, index) => {
             return start + (newViewBox[index] - start) * progress;
+        });
+
+        svg.setAttribute('viewBox', intermediateViewBox.join(' '));
+
+        if (progress < 1) {
+            requestAnimationFrame(animateZoom);
+        }
+    }
+
+    requestAnimationFrame(animateZoom);
+}
+
+function easeInOutQuad(t) {
+    return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+}
+
+function zoomOut() {
+    var svg = document.getElementById('map');
+    var currentViewBox = svg.getAttribute('viewBox').split(' ').map(Number);
+    var defaultViewBox = [-221.46, -221.46, 1822.433, 911.283];
+
+    // Duration of the animation in milliseconds
+    var duration = 1000;
+    var startTime = null;
+
+    function animateZoom(timestamp) {
+        if (!startTime) startTime = timestamp;
+        var elapsedTime = timestamp - startTime;
+        var progress = Math.min(elapsedTime / duration, 1);
+
+        // Apply an ease-in-out function to the progress
+        progress = easeInOutQuad(progress);
+
+        // Interpolate between the current and default viewBox values
+        var intermediateViewBox = currentViewBox.map(function (start, index) {
+            return start + (defaultViewBox[index] - start) * progress;
         });
 
         svg.setAttribute('viewBox', intermediateViewBox.join(' '));
