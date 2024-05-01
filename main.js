5824c90b5f1bfd5a6b7d39509418246f4b8a8b97
unknown <chetansharma2-deloitte>
2024-05-01 14:08:49 +0530

    h

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 
 // const mongoURI = 'mongodb://localhost:27017';
-const mongoURI = 'mongodb+srv://cs7804:cs7804@cluster0.aqk4ehm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoURI = process.env.mongoDb
 
 
 // const client = new MongoClient(mongoURI);
